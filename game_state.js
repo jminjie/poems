74292962ac3231604d23af39ddb40a186b4f7076
Jordan <jminjie@umich.edu>
2018-11-24 23:35:29 -0800

    Implement get poem

--- game_state.js
+++ game_state.js
@@ -5,6 +5,7 @@ const SERVER_URL = 'http://192.168.86.84:8080/';
 const GAME_STATE_URL = 'getGameState';
 const INCREMENT_URL = 'nextState';
 const SUBMIT_POEM_URL = 'submitFullPoem';
+const POEM_URL = 'getPoem';
 
 // just for testing
 function setGameState(state) {
@@ -24,6 +25,10 @@ function getGameState() {
     */
 }
 
+function getPoem() {
+    return fetch(SERVER_URL + POEM_URL);
+}
+
 function incrementGameState() {
    return fetch(SERVER_URL + INCREMENT_URL);
 }
@@ -34,35 +39,4 @@ function sendPoemRequest(submission) {
             method: "POST",
             body: submission
         });
-}
-
-const POEM = `
-Gaily bedight,
-   A gallant knight, 
-In sunshine and in shadow,   
-   Had journeyed long,   
-   Singing a song, 
-In search of Eldorado. 
-
-   But he grew old— 
-   This knight so bold—   
-And o’er his heart a shadow—   
-   Fell as he found 
-   No spot of ground 
-That looked like Eldorado. 
-
-   And, as his strength   
-   Failed him at length, 
-He met a pilgrim shadow—   
-   ‘Shadow,’ said he,   
-   ‘Where can it be— 
-This land of Eldorado?’ 
-`;
-
-function getPoem() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(POEM);
-        }, 1000);
-    });
 }
