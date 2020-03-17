43c5bd40baa6c8bb3deb621812781fb7b7bdc692
Mark Liu <uilkram@gmail.com>
2020-03-16 23:34:35 -0700

    move state into map

--- server/main.go
+++ server/main.go
@@ -1,13 +1,14 @@
 package main
+
 import (
-  "net/http"
-  "strings"
-  "strconv"
-  "io/ioutil"
-  "fmt"
-  "flag"
-  "encoding/json"
-  //  "errors"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 var STATE_POEM_SUBMISSION = 0
@@ -15,126 +16,146 @@ var STATE_VERSE_SUBMISSION = 1
 var STATE_VERSE_VOTE = 2
 var STATE_POEM_REVEAL = 3
 
-var currentGameState = 0
-var fullPoem = "No poem selected yet"
-var hiddenPoem []string
-var poemStart string
-var poemRealEnding string
-var endings []string
+// each of the following globals represent some state of the game, keyed by
+// game key
+var currentGameState = make(map[string]int)
+var fullPoem = make(map[string]string)
+var poemStart = make(map[string]string)
+var poemRealEnding = make(map[string]string)
+var endings = make(map[string][]string)
 
 // Flags
 var portvar int
+
 func init() {
-     flag.IntVar(&portvar, "port", 8080, "help message for flagname")
+	flag.IntVar(&portvar, "port", 8080, "help message for flagname")
 }
 
 func enableCors(w *http.ResponseWriter) {
-     (*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func getKeyFromRequest(r *http.Request) (string, error) {
+	keys, ok := r.URL.Query()["key"]
+	if !ok || len(keys) != 1 {
+		return "", errors.New("Missing url query param 'key'")
+	}
+	key := keys[0]
+	return key, nil
 }
 
 // Splits poem into start and ending and store them in poemStart and poemRealEnding.
 // Returns false if error occurs.
-func parsePoem(poem string) bool {
-  var emptyLineIdxs []int
-  poem = strings.TrimSpace(poem)
-  var poemLines = strings.Split(poem, "\n")
-  fmt.Print(strings.Join(poemLines, ", "))
-  for i:= 0; i < len(poemLines); i++ {
-      if len(poemLines[i]) == 0 {
-      	 emptyLineIdxs = append(emptyLineIdxs, i)
-      }     	 
-  }
-  
-  if len(emptyLineIdxs) < 1 {
-    fmt.Print("poem doesn't have any empty lines")
-    return false
-  }
+func parsePoem(poem string, key string) bool {
+	var emptyLineIdxs []int
+	poem = strings.TrimSpace(poem)
+	var poemLines = strings.Split(poem, "\n")
+	fmt.Print(strings.Join(poemLines, ", "))
+	for i := 0; i < len(poemLines); i++ {
+		if len(poemLines[i]) == 0 {
+			emptyLineIdxs = append(emptyLineIdxs, i)
+		}
+	}
 
-  // what's the last stanza?
-  // it should be the last new line after which there are non-empty lines.
-  lineBreakIdxToUse := emptyLineIdxs[len(emptyLineIdxs)-1]
-  poemStart = strings.Join(poemLines[0:lineBreakIdxToUse], "\n") + "\n"
-  poemRealEnding = strings.Join(poemLines[lineBreakIdxToUse+1:], "\n")
-  return true
+	if len(emptyLineIdxs) < 1 {
+		fmt.Print("poem doesn't have any empty lines")
+		return false
+	}
+
+	// what's the last stanza?
+	// it should be the last new line after which there are non-empty lines.
+	lineBreakIdxToUse := emptyLineIdxs[len(emptyLineIdxs)-1]
+	poemStart[key] = strings.Join(poemLines[0:lineBreakIdxToUse], "\n") + "\n"
+	poemRealEnding[key] = strings.Join(poemLines[lineBreakIdxToUse+1:], "\n")
+	return true
 }
 
 func submitFullPoemHandler(w http.ResponseWriter, r *http.Request) {
-  enableCors(&w)
-  body, _ := ioutil.ReadAll(r.Body)
-  fullPoem = string(body)
-  parsePoem(fullPoem)
-  w.Write([]byte(poemStart))
+	enableCors(&w)
+	key, _ := getKeyFromRequest(r)
+	fmt.Print("key:" + key)
+	body, _ := ioutil.ReadAll(r.Body)
+	fullPoem[key] = string(body)
+	parsePoem(fullPoem[key], key)
+	w.Write([]byte(poemStart[key]))
 }
 
 func getGameStateHandler(w http.ResponseWriter, r *http.Request) {
-  enableCors(&w)
-  w.Write([]byte(strconv.Itoa(currentGameState)))
+	enableCors(&w)
+	key, _ := getKeyFromRequest(r)
+	w.Write([]byte(strconv.Itoa(currentGameState[key])))
 }
 
 // DEPRECATED
 func nextStateHandler(w http.ResponseWriter, r *http.Request) {
-  enableCors(&w)
-  currentGameState += 1
-  if (currentGameState >= 4) {
-    currentGameState = 0
-  }
+	enableCors(&w)
+	key, _ := getKeyFromRequest(r)
+	currentGameState[key] += 1
+	if currentGameState[key] >= 4 {
+		currentGameState[key] = 0
+	}
 }
 
 func setStateHandler(w http.ResponseWriter, r *http.Request) {
-  enableCors(&w)
-  body, readErr := ioutil.ReadAll(r.Body)
-  if readErr != nil {
-    return
-  }
-  state, err := strconv.Atoi(string(body))
-  if err != nil {
-    return
-  }
-  if state < 0 || state > 4 {
-    return
-  }
-  currentGameState = state
+	enableCors(&w)
+	key, _ := getKeyFromRequest(r)
+	body, readErr := ioutil.ReadAll(r.Body)
+	if readErr != nil {
+		return
+	}
+	state, err := strconv.Atoi(string(body))
+	if err != nil {
+		return
+	}
+	if state < 0 || state > 4 {
+		return
+	}
+	currentGameState[key] = state
 }
 
 func submitEndingHandler(w http.ResponseWriter, r *http.Request) {
-  enableCors(&w)
-  body, _ := ioutil.ReadAll(r.Body)
-  endings = append(endings, string(body))
+	enableCors(&w)
+	key, _ := getKeyFromRequest(r)
+	body, _ := ioutil.ReadAll(r.Body)
+	endings[key] = append(endings[key], string(body))
 }
 
 func getPoemHandler(w http.ResponseWriter, r *http.Request) {
-  enableCors(&w)
-  w.Write([]byte(poemStart))
+	enableCors(&w)
+	key, _ := getKeyFromRequest(r)
+	w.Write([]byte(poemStart[key]))
 }
 
 func getEndingsHandler(w http.ResponseWriter, r *http.Request) {
-  enableCors(&w)
-  resp := make(map[string] []string)
-  resp["endings"] = append(resp["endings"], poemRealEnding)
-  for _, ending := range endings {
-    resp["endings"] = append(resp["endings"], ending)
-  }
-  bytes, _ := json.Marshal(resp)
-  w.Write(bytes)
+	enableCors(&w)
+	key, _ := getKeyFromRequest(r)
+	resp := make(map[string][]string)
+	resp["endings"] = append(resp["endings"], poemRealEnding[key])
+	for _, ending := range endings[key] {
+		resp["endings"] = append(resp["endings"], ending)
+	}
+	bytes, _ := json.Marshal(resp)
+	w.Write(bytes)
 }
 
 func getRealEndingHandler(w http.ResponseWriter, r *http.Request) {
-  enableCors(&w)
-  w.Write([]byte(poemRealEnding))
+	enableCors(&w)
+	key, _ := getKeyFromRequest(r)
+	w.Write([]byte(poemRealEnding[key]))
 }
 
 func main() {
-  flag.Parse()
-  http.HandleFunc("/getGameState", getGameStateHandler)
-  http.HandleFunc("/submitFullPoem", submitFullPoemHandler)
-  http.HandleFunc("/nextState", nextStateHandler)
-  http.HandleFunc("/setState", setStateHandler)
-  http.HandleFunc("/submitEnding", submitEndingHandler)
-  http.HandleFunc("/getPoem", getPoemHandler)
-  http.HandleFunc("/getEndings", getEndingsHandler)
-  http.HandleFunc("/getRealEnding", getRealEndingHandler)
+	flag.Parse()
+	http.HandleFunc("/getGameState", getGameStateHandler)
+	http.HandleFunc("/submitFullPoem", submitFullPoemHandler)
+	http.HandleFunc("/nextState", nextStateHandler)
+	http.HandleFunc("/setState", setStateHandler)
+	http.HandleFunc("/submitEnding", submitEndingHandler)
+	http.HandleFunc("/getPoem", getPoemHandler)
+	http.HandleFunc("/getEndings", getEndingsHandler)
+	http.HandleFunc("/getRealEnding", getRealEndingHandler)
 
-  if err := http.ListenAndServe(":"+strconv.Itoa(portvar), nil); err != nil {
-    panic(err)
-  }
+	if err := http.ListenAndServe(":"+strconv.Itoa(portvar), nil); err != nil {
+		panic(err)
+	}
 }
--- server_utils.js
+++ server_utils.js
@@ -1,4 +1,4 @@
-//const SERVER_URL = 'http://192.168.86.84:8080/';
+// const SERVER_URL = 'http://192.168.86.84:8080/';
 const SERVER_URL = 'http://localhost:8080/';
 
 const GAME_STATE_URL = 'getGameState';
@@ -9,29 +9,35 @@ const SUBMIT_ENDING_URL = 'submitEnding';
 const ENDINGS_URL = 'getEndings';
 const REAL_ENDING_URL = 'getRealEnding';
 
+const KEY_SUFFIX = '?key=ABC'
+
 // just for testing
 const TEST_MODE = false;
 
-function setGameState(state) {
-    testGameState = state
-}
-var testGameStatePromise = {
-    json() {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve(testGameState);
-            }, 500);
-        });
+function keySuffix() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var entries = urlParams.entries();
+  for (pair of entries) {
+    if (pair[0] == "key") {
+      return "?key=" + pair[1];
     }
+  }
+  console.log("Missing key");
+  return "";
+}
+
+function setGameState(state) { testGameState = state }
+var testGameStatePromise = {
+  json() {
+    return new Promise(
+        resolve => { setTimeout(() => { resolve(testGameState); }, 500); });
+  }
 };
 var testPoemPromise = {
-    json() {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve(TEST_POEM);
-            }, 500);
-        });
-    }
+  json() {
+    return new Promise(
+        resolve => { setTimeout(() => { resolve(TEST_POEM); }, 500); });
+  }
 };
 const TEST_POEM = `Gaily bedight,
    A gallant knight,
@@ -55,58 +61,55 @@ He met a pilgrim shadow—
 This land of Eldorado?’`;
 // end of testing section
 
-
 async function getGameState() {
-    if (!TEST_MODE) {
-        let result = await fetch(SERVER_URL + GAME_STATE_URL);
-        return await result.text();
-    } else {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve(testGameStatePromise);
-            }, 500);
-        });
-    }
+  if (!TEST_MODE) {
+    let result = await fetch(SERVER_URL + GAME_STATE_URL + keySuffix());
+    return await result.text();
+  } else {
+    return new Promise(resolve => {
+      setTimeout(() => { resolve(testGameStatePromise); }, 500);
+    });
+  }
 }
 
 async function getPoem() {
-    if (!TEST_MODE) {
-        let result = await fetch(SERVER_URL + POEM_URL);
-        return await result.text();
-    } else {
-        return testPoemPromise;
-    }
+  if (!TEST_MODE) {
+    let result = await fetch(SERVER_URL + POEM_URL + keySuffix());
+    return await result.text();
+  } else {
+    return testPoemPromise;
+  }
 }
 
 async function getEndings() {
-    if (!TEST_MODE) {
-        let result = await fetch(SERVER_URL + ENDINGS_URL);
-        let resultJson = await result.json();
-        return resultJson['endings'];
-    }
+  if (!TEST_MODE) {
+    let result = await fetch(SERVER_URL + ENDINGS_URL + keySuffix());
+    let resultJson = await result.json();
+    return resultJson['endings'];
+  }
 }
 
 async function getRealEnding() {
-    if (!TEST_MODE) {
-        let result = await fetch(SERVER_URL + REAL_ENDING_URL);
-        return await result.text();
-    }
+  if (!TEST_MODE) {
+    let result = await fetch(SERVER_URL + REAL_ENDING_URL + keySuffix());
+    return await result.text();
+  }
 }
 
 function incrementGameState() {
-   return fetch(SERVER_URL + INCREMENT_URL);
+  return fetch(SERVER_URL + INCREMENT_URL + keySuffix());
 }
 
 function sendPoemRequest(submission) {
-    if (!TEST_MODE) {
-        return fetch(SERVER_URL + SUBMIT_POEM_URL,
-            { method: "POST", body: submission });
-    }
+  if (!TEST_MODE) {
+    return fetch(SERVER_URL + SUBMIT_POEM_URL + keySuffix(),
+                 {method : "POST", body : submission});
+  }
 }
 
 function sendEndingRequest(ending) {
-    if (!TEST_MODE) {
-        return fetch(SERVER_URL + SUBMIT_ENDING_URL,
-            { method: "POST", body: ending });
-    }
+  if (!TEST_MODE) {
+    return fetch(SERVER_URL + SUBMIT_ENDING_URL + keySuffix(),
+                 {method : "POST", body : ending});
+  }
 }
